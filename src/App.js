d84834d7c0bd2978f9372bdd49760d6cdfd7152a
alex-stout <stoutab4384@uwec.edu>
2018-07-03 18:56:36 -0500

    moved around data so it makes sense chronologically

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import BarChart from './components/BarChart'
+import TotalCost from './components/TotalCost'
 
 class App extends Component {
   render() {
-    return (<div className="App">
-      <h1>Student Senate Fee Report</h1>
-      <BarChart/>
-    </div>);
+    return (
+        <div className="App">
+          <h1>Student Senate Fee Report</h1>
+          <p>This page is brought to you by UWEC Student Senate as a collaboration between the Finance and Information Technology Comissions.</p>
+          <h2>Segregated Fee Spending by Activity</h2>
+          <BarChart/>
+          <TotalCost/>
+        </div>
+  );
   }
 }
 
--- src/components/TotalCost.js
+++ src/components/TotalCost.js
@@ -1,11 +1,76 @@
-import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
+import React from 'react';
+import { Line as LineChart } from 'react-chartjs-2';
 
-class TotalCost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // data goes here
-    }
+function chartData() {
+  return {
+    labels: ['2015', '2016', '2017', '2018'],
+    datasets: [
+      {
+        type: 'line',
+        label: 'Total Cost',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [1260.95, 1271.48, 1454.69, 1459.41],
+      }
+    ]
   }
 }
+
+const options = {
+  responsive: true,
+  scaleShowGridLines: true,
+  scaleGridLineColor: 'rgba(0,0,0,.05)',
+  scaleGridLineWidth: 1,
+  scaleShowHorizontalLines: true,
+  scaleShowVerticalLines: true,
+  bezierCurve: true,
+  bezierCurveTension: 0,
+  pointDot: true,
+  pointDotRadius: 4,
+  pointDotStrokeWidth: 10,
+  pointHitDetectionRadius: 80,
+  datasetStroke: true,
+  datasetStrokeWidth: 2,
+  datasetFill: false,
+  displayLegend: true,
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+}
+
+const styles = {
+  graphContainer: {
+    border: '1px solid black',
+    padding: '15px'
+  }
+}
+
+class TotalCost extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: chartData()
+    }
+  }
+
+  render() {
+    return (
+      <div style={styles.graphContainer}>
+        <LineChart data={this.state.data}
+          options={options}
+          width="600" height="250"/>
+      </div>
+    )
+  }
+}
+
+export default TotalCost;
