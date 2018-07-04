d7b0e40fa989ccec39e85c8ea74bc7e882767520
alex-stout <stoutab4384@uwec.edu>
2018-07-03 20:47:14 -0500

    remove default and add a little info

---
+++ src/components/FeeTable.js
@@ -0,0 +1,276 @@
+import React, {Component} from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-boostrap-table';
+
+class FeeTable extends Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: {
+        products: [
+          {
+            activity: "Davies Student Center",
+            y2015: "$326.00",
+            y2016: "$326.00",
+            y2017: "$326.00",
+            y2018: "$326.00"
+          },
+          {
+            activity: "University Centers",
+            y2015: "$218.00",
+            y2016: "$225.00",
+            y2017: "$235.00",
+            y2018: "$245.00"
+          },
+          {
+            activity: "University Recreation & Sports Facilities",
+            y2015: "$178.96"
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Student Health Service",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Text Rental",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Organized Activities  (total fee)",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Athletics",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Transit",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Counseling Services",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Music",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Student Office of Sustainability",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Children's Nature Academy  (facility)",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Student Senate General Operations",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Children's Nature Academy",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Theatre",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Intramurals",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Municipal Services",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "University Activities Commission",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Club Sports",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Forum",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Artist Series",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Forensics",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Student Organizations",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Visual Arts",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Women's & LGBTQ Resource Center",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Athletics / Recreation Facilities Improvements",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "UWEC Radio",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Legal Services",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "International Activities",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Spectator",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Organized Activities Reserve",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Pow Wow",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "NOTA",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Flip Side",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "International Films",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "Readership Program",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          },
+          {
+            activity: "CASE",
+            y2015:
+            y2016:
+            y2017:
+            y2018:
+          }
+        ]
+      }
+    }
+  }
+}
