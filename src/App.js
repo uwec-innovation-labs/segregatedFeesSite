2ca2e700676b3f6dec2dfdcc120f1fa604f4264a
alex-stout <stoutab4384@uwec.edu>
2018-07-03 19:56:26 -0500

    change name of app and favicon

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App extends Component {
         <div className="App">
           <h1>Student Senate Fee Report</h1>
           <p>This page is brought to you by UWEC Student Senate as a collaboration between the Finance and Information Technology Comissions.</p>
+
+          <h3>What are Segreated Fees?</h3>
+          <p>Segregated fees provide funds for cultural, recreational, and leisure activities and groups that are not funded through other state appropriations. They are intended to contribute to the richness of the university community. Segregated fees are not user fees. </p>
+
+
           <h2>Segregated Fee Spending by Activity</h2>
           <BarChart/>
           <TotalCost/>
