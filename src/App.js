815b55b38f43067cf3546c1c43beeacc4edff5d4
alex-stout <stoutab4384@uwec.edu>
2018-07-06 00:07:25 -0500

    got button filter working but it's not a good way to do it

--- src/App.js
+++ src/App.js
@@ -48,25 +48,12 @@ class App extends Component {
 
   filterNonAllocable(e) {
     // TO-DO replace with API call sourced from data.csv
-    var nonallocables = [
-      'Davies Student Center',
-      'University Centers',
-      'University Recreation & Sports Facilities',
-      'Student Health Service',
-      'Text Rental',
-      'Athletics',
-      'Transit',
-      'Counseling Services',
-      'Children\'s Nature Academy',
-      'Children\'s Nature Academy Building'
-    ]
     var allocables = [
       'Music',
       'Student Office of Sustainability',
       'Student Senate General Operations',
       'Theatre',
       'Intramurals',
-      'Municipal Services',
       'University Activities Commission',
       'Club Sports',
       'Forum',
@@ -88,20 +75,42 @@ class App extends Component {
       'Readership Program',
       'CASE'
     ]
-    for (var i = 0; i < allocables.length; i++) {
-      var index = this.state.barChartData.labels.indexOf(allocables[i]);
-      if (index > -1) {
-        console.log("Found a match for " + allocables[i])
-        // pop off label, 2015, 2016, 2017, 2018
-        console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
-        this.state.barChartData.datasets.forEach((dataset) => {
-          console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
-        })
-      } else {
-        console.log(allocables[i] + " didn't match " + this.state.barChartData.labels[index])
+    fetch('https://3b6gdit4v0.execute-api.us-east-2.amazonaws.com/latest/v0')
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      // clear the state
+      this.state.barChartData.labels = [];
+      this.state.barChartData.datasets[3].data = []
+      this.state.barChartData.datasets[2].data = []
+      this.state.barChartData.datasets[1].data = []
+      this.state.barChartData.datasets[0].data = []
+
+      // extract the data for each year and the activity list
+      for (var i = 0; i < data.length - 1; i++) {
+        this.state.barChartData.labels.push(data[i].activity)
+        this.state.barChartData.datasets[3].data.push(data[i][2015])
+        this.state.barChartData.datasets[2].data.push(data[i][2016])
+        this.state.barChartData.datasets[1].data.push(data[i][2017])
+        this.state.barChartData.datasets[0].data.push(data[i][2018])
       }
-    }
-    this.setState({ isLoaded: true });
+      
+      for (var i = 0; i < allocables.length; i++) {
+        var index = this.state.barChartData.labels.indexOf(allocables[i]);
+        if (index > -1) {
+          console.log("Found a match for " + allocables[i])
+          // pop off label, 2015, 2016, 2017, 2018
+          console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
+          this.state.barChartData.datasets.forEach((dataset) => {
+            console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
+          })
+        } else {
+          console.log(allocables[i] + " didn't match " + this.state.barChartData.labels[index])
+        }
+      }
+      this.setState({ isLoaded: true });
+    })
+
 }
 
   filterAllocable(e) {
@@ -113,53 +122,50 @@ class App extends Component {
       'Student Health Service',
       'Text Rental',
       'Athletics',
+      'Municipal Services',
       'Transit',
       'Counseling Services',
       'Children\'s Nature Academy',
       'Children\'s Nature Academy Building'
     ]
-    var allocables = [
-      'Music',
-      'Student Office of Sustainability',
-      'Student Senate General Operations',
-      'Theatre',
-      'Intramurals',
-      'Municipal Services',
-      'University Activities Commission',
-      'Club Sports',
-      'Forum',
-      'Artist Series',
-      'Forensics',
-      'Student Organizations',
-      'Visual Arts',
-      'Women\'s & LGBTQ Resource Center',
-      'Athletics / Recreation Facilities Improvements',
-      'UWEC Radio',
-      'Legal Services',
-      'International Activities',
-      'Spectator',
-      'Organized Activities Reserve',
-      'Pow Wow',
-      'NOTA',
-      'Flip Side',
-      'International Films',
-      'Readership Program',
-      'CASE'
-    ]
-    for (var i = 0; i < nonallocables.length; i++) {
-      var index = this.state.barChartData.labels.indexOf(nonallocables[i]);
-      if (index > -1) {
-        console.log("Found a match for " + nonallocables[i])
-        // pop off label, 2015, 2016, 2017, 2018
-        console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
-        this.state.barChartData.datasets.forEach((dataset) => {
-          console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
-        })
-      } else {
-        console.log(nonallocables[i] + " didn't match " + this.state.barChartData.labels[index])
+    fetch('https://3b6gdit4v0.execute-api.us-east-2.amazonaws.com/latest/v0')
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      // clear the state
+      this.state.barChartData.labels = [];
+      this.state.barChartData.datasets[3].data = []
+      this.state.barChartData.datasets[2].data = []
+      this.state.barChartData.datasets[1].data = []
+      this.state.barChartData.datasets[0].data = []
+
+      // extract the data for each year and the activity list
+      for (var i = 0; i < data.length - 1; i++) {
+        this.state.barChartData.labels.push(data[i].activity)
+        this.state.barChartData.datasets[3].data.push(data[i][2015])
+        this.state.barChartData.datasets[2].data.push(data[i][2016])
+        this.state.barChartData.datasets[1].data.push(data[i][2017])
+        this.state.barChartData.datasets[0].data.push(data[i][2018])
       }
-    }
-    this.setState({ isLoaded: true });
+
+      for (var i = 0; i < nonallocables.length; i++) {
+        var index = this.state.barChartData.labels.indexOf(nonallocables[i]);
+        if (index > -1) {
+          console.log("Found a match for " + nonallocables[i])
+          // pop off label, 2015, 2016, 2017, 2018
+          console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
+          this.state.barChartData.datasets.forEach((dataset) => {
+            console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
+          })
+        } else {
+          console.log(nonallocables[i] + " didn't match " + this.state.barChartData.labels[index])
+        }
+      }
+      this.setState({ isLoaded: true });
+      // now construct the datasets with data
+      this.setState({ isLoaded: true })
+    })
+
 }
 
   getBarChartData() {
@@ -205,7 +211,7 @@ class App extends Component {
             <h3>Segregated Fee Spending by Activity</h3>
             <button  type="button" className="btn btn-outline-danger" onClick={this.getBarChartData.bind(this)}>All</button>
             <button type="button" className="btn btn-outline-danger" onClick={this.filterAllocable.bind(this)}>Allocable</button>
-            <button type="button" className="btn btn-outline-danger" onClick={this.filterNonAllocable.bind}>Non-Allocable</button>
+            <button type="button" className="btn btn-outline-danger" onClick={this.filterNonAllocable.bind(this)}>Non-Allocable</button>
             <p><small>Click on the years to compare</small></p>
             <BarChart redraw={true} chartData={this.state.barChartData}/>
             <h3 id="percentage">Spending as Percentage of Total</h3>
