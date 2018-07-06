6e54dbf8a4277cfa48bb087dd5ff1a6fa6e89a36
alex-stout <stoutab4384@uwec.edu>
2018-07-05 22:29:33 -0500

    UI changes & API call in app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,85 @@ import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'reactstrap';
+import fetch from 'isomorphic-fetch';
 import BarChart from './components/BarChart'
 import PieChart from './components/PieChart'
 
 import logo from './logo.png'
 
+var barColors = ['#E83E45', '#FEDF03', '#00D49D', '#0085B6']
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rawData: {},
+      barChartData: {
+        labels: [],
+        datasets: [
+          {
+            label: '2018-2019',
+            data: [],
+            backgroundColor: barColors[0]
+          },
+          {
+            label: '2017-2018',
+            data: [],
+            backgroundColor: barColors[1],
+            hidden: true
+          },
+          {
+            label: '2016-2017',
+            data: [],
+            backgroundColor: barColors[2],
+            hidden: true
+          },
+          {
+            label: '2015-2016',
+            data: [],
+            backgroundColor: barColors[3],
+            hidden: true
+          },
+        ]},
+      pieChartData: {}
+    }
+  }
+
+  updateBarChart() {
+
+  }
+
+  filterAllocable() {
+    var allocables = ['Student Senate General Operations']
+
+    for (var i = 0; i < this.state.barChartData; i++) {
+
+    }
+  }
+
+  getBarChartData() {
+    // AJAX call
+    fetch('https://3b6gdit4v0.execute-api.us-east-2.amazonaws.com/latest/v0')
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      // extract the data for each year and the activity list
+      for (var i = 0; i < data.length - 1; i++) {
+        this.state.barChartData.labels.push(data[i].activity)
+        this.state.barChartData.datasets[3].data.push(data[i][2015])
+        this.state.barChartData.datasets[2].data.push(data[i][2016])
+        this.state.barChartData.datasets[1].data.push(data[i][2017])
+        this.state.barChartData.datasets[0].data.push(data[i][2018])
+      }
+      // now construct the datasets with data
+      this.setState({ isLoaded: true })
+    })
+  }
+
+  componentWillMount() {
+    this.getBarChartData();
+  }
+
   render() {
     return (
         <div className="App" height="100%">
@@ -19,8 +92,11 @@ class App extends Component {
               <p>Segregated fees provide funds for cultural, recreational, and leisure activities and groups that are not funded through other state appropriations. They are intended to contribute to the richness of the university community. Segregated fees are not user fees. </p>
             </div>
             <h3>Segregated Fee Spending by Activity</h3>
+            <button onClick={null}>All</button>
+            <button>Allocable</button>
+            <button>Non-Allocable</button>
             <p><small>Click on the years to compare</small></p>
-            <BarChart/>
+            <BarChart chartData={this.state.barChartData}/>
             <h3 id="percentage">Spending as Percentage of Total</h3>
             <div className="pieHolder">
               <PieChart/>
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,61 +1,12 @@
 import React, {Component} from 'react';
 import {HorizontalBar} from 'react-chartjs-2';
-import fetch from 'isomorphic-fetch';
-
-var colors = ['#E83E45', '#FEDF03', '#00D49D', '#0085B6']
 
 class BarChart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: false,
-      chartData: {
-        labels: [],
-        datasets: [
-          {
-            label: '2018-2019',
-            data: [],
-            backgroundColor: colors[0]
-          },
-          {
-            label: '2017-2018',
-            data: [],
-            backgroundColor: colors[1],
-            hidden: true
-          },
-          {
-            label: '2016-2017',
-            data: [],
-            backgroundColor: colors[2],
-            hidden: true
-          },
-          {
-            label: '2015-2016',
-            data: [],
-            backgroundColor: colors[3],
-            hidden: true
-          },
-        ]
-      },
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://3b6gdit4v0.execute-api.us-east-2.amazonaws.com/latest/v0')
-    .then(res => {
-      return res.json();
-    }).then(data => {
-      // extract the data for each year and the activity list
-      for (var i = 0; i < data.length - 1; i++) {
-        this.state.chartData.labels.push(data[i].activity)
-        this.state.chartData.datasets[3].data.push(data[i][2015])
-        this.state.chartData.datasets[2].data.push(data[i][2016])
-        this.state.chartData.datasets[1].data.push(data[i][2017])
-        this.state.chartData.datasets[0].data.push(data[i][2018])
+      chartData: props.chartData
       }
-      // now construct the datasets with data
-      this.setState({ isLoaded: true })
-    })
   }
 
   static defaultProps = {
@@ -66,7 +17,11 @@ class BarChart extends Component {
 
   render() {
     return (<div className="chart">
+<<<<<<< Updated upstream
       {this.state.isLoaded ? <HorizontalBar data={this.state.chartData} height={400} options={{
+=======
+    <HorizontalBar data={this.state.chartData} height={500} options={{
+>>>>>>> Stashed changes
           legend: {
             display: this.props.displayLegend,
             position: this.props.legendPosition,
@@ -92,7 +47,6 @@ class BarChart extends Component {
             }]
           }
         }}/>
-      : <div>Loading data...</div> }
     </div>)
   }
 }
