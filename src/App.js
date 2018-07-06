2ce811f23b5c350ee9b4307586414b1659077763
alex-stout <stoutab4384@uwec.edu>
2018-07-05 23:41:49 -0500

    got filter for all and allocable working

--- src/App.js
+++ src/App.js
@@ -46,17 +46,121 @@ class App extends Component {
     }
   }
 
-  updateBarChart() {
-
-  }
-
-  filterAllocable() {
-    var allocables = ['Student Senate General Operations']
-
-    for (var i = 0; i < this.state.barChartData; i++) {
-
+  filterNonAllocable(e) {
+    // TO-DO replace with API call sourced from data.csv
+    var nonallocables = [
+      'Davies Student Center',
+      'University Centers',
+      'University Recreation & Sports Facilities',
+      'Student Health Service',
+      'Text Rental',
+      'Athletics',
+      'Transit',
+      'Counseling Services',
+      'Children\'s Nature Academy',
+      'Children\'s Nature Academy Building'
+    ]
+    var allocables = [
+      'Music',
+      'Student Office of Sustainability',
+      'Student Senate General Operations',
+      'Theatre',
+      'Intramurals',
+      'Municipal Services',
+      'University Activities Commission',
+      'Club Sports',
+      'Forum',
+      'Artist Series',
+      'Forensics',
+      'Student Organizations',
+      'Visual Arts',
+      'Women\'s & LGBTQ Resource Center',
+      'Athletics / Recreation Facilities Improvements',
+      'UWEC Radio',
+      'Legal Services',
+      'International Activities',
+      'Spectator',
+      'Organized Activities Reserve',
+      'Pow Wow',
+      'NOTA',
+      'Flip Side',
+      'International Films',
+      'Readership Program',
+      'CASE'
+    ]
+    for (var i = 0; i < allocables.length; i++) {
+      var index = this.state.barChartData.labels.indexOf(allocables[i]);
+      if (index > -1) {
+        console.log("Found a match for " + allocables[i])
+        // pop off label, 2015, 2016, 2017, 2018
+        console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
+        this.state.barChartData.datasets.forEach((dataset) => {
+          console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
+        })
+      } else {
+        console.log(allocables[i] + " didn't match " + this.state.barChartData.labels[index])
+      }
     }
-  }
+    this.setState({ isLoaded: true });
+}
+
+  filterAllocable(e) {
+    // TO-DO replace with API call sourced from data.csv
+    var nonallocables = [
+      'Davies Student Center',
+      'University Centers',
+      'University Recreation & Sports Facilities',
+      'Student Health Service',
+      'Text Rental',
+      'Athletics',
+      'Transit',
+      'Counseling Services',
+      'Children\'s Nature Academy',
+      'Children\'s Nature Academy Building'
+    ]
+    var allocables = [
+      'Music',
+      'Student Office of Sustainability',
+      'Student Senate General Operations',
+      'Theatre',
+      'Intramurals',
+      'Municipal Services',
+      'University Activities Commission',
+      'Club Sports',
+      'Forum',
+      'Artist Series',
+      'Forensics',
+      'Student Organizations',
+      'Visual Arts',
+      'Women\'s & LGBTQ Resource Center',
+      'Athletics / Recreation Facilities Improvements',
+      'UWEC Radio',
+      'Legal Services',
+      'International Activities',
+      'Spectator',
+      'Organized Activities Reserve',
+      'Pow Wow',
+      'NOTA',
+      'Flip Side',
+      'International Films',
+      'Readership Program',
+      'CASE'
+    ]
+    for (var i = 0; i < nonallocables.length; i++) {
+      var index = this.state.barChartData.labels.indexOf(nonallocables[i]);
+      if (index > -1) {
+        console.log("Found a match for " + nonallocables[i])
+        // pop off label, 2015, 2016, 2017, 2018
+        console.log('Removing label' + this.state.barChartData.labels.splice(index, 1));
+        this.state.barChartData.datasets.forEach((dataset) => {
+          console.log("Removing " + dataset.data.splice(index, 1) + " dataset.");
+        })
+      } else {
+        console.log(nonallocables[i] + " didn't match " + this.state.barChartData.labels[index])
+      }
+    }
+    this.setState({ isLoaded: true });
+}
 
   getBarChartData() {
     // AJAX call
@@ -64,6 +168,13 @@ class App extends Component {
     .then(res => {
       return res.json();
     }).then(data => {
+      // clear the state
+      this.state.barChartData.labels = [];
+      this.state.barChartData.datasets[3].data = []
+      this.state.barChartData.datasets[2].data = []
+      this.state.barChartData.datasets[1].data = []
+      this.state.barChartData.datasets[0].data = []
+
       // extract the data for each year and the activity list
       for (var i = 0; i < data.length - 1; i++) {
         this.state.barChartData.labels.push(data[i].activity)
@@ -92,11 +203,11 @@ class App extends Component {
               <p>Segregated fees provide funds for cultural, recreational, and leisure activities and groups that are not funded through other state appropriations. They are intended to contribute to the richness of the university community. Segregated fees are not user fees. </p>
             </div>
             <h3>Segregated Fee Spending by Activity</h3>
-            <button onClick={null}>All</button>
-            <button>Allocable</button>
-            <button>Non-Allocable</button>
+            <button  type="button" className="btn btn-outline-danger" onClick={this.getBarChartData.bind(this)}>All</button>
+            <button type="button" className="btn btn-outline-danger" onClick={this.filterAllocable.bind(this)}>Allocable</button>
+            <button type="button" className="btn btn-outline-danger" onClick={this.filterNonAllocable.bind}>Non-Allocable</button>
             <p><small>Click on the years to compare</small></p>
-            <BarChart chartData={this.state.barChartData}/>
+            <BarChart redraw={true} chartData={this.state.barChartData}/>
             <h3 id="percentage">Spending as Percentage of Total</h3>
             <div className="pieHolder">
               <PieChart/>
